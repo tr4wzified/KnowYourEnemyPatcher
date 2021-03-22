0fcaf666d71c2ab7102891dea53bdabaacb4e772
tr4wzified <55751269+tr4wzified@users.noreply.github.com>
2021-03-22 13:50:38 +0100

    Update ModKey.cs

--- KnowYourEnemyMutagen/FormKeys/KnowYourEnemy/ModKey.cs
+++ KnowYourEnemyMutagen/FormKeys/KnowYourEnemy/ModKey.cs
@@ -2,6 +2,6 @@ namespace Mutagen.Bethesda.FormKeys.SkyrimSE
 {
     public static partial class KnowYourEnemy
     {
-        public readonly static ModKey ModKey = ModKey.FromNameAndExtension("KnowYourEnemy.esp");
+        public readonly static ModKey ModKey = ModKey.FromNameAndExtension("know_your_enemy.esp");
     }
 }
