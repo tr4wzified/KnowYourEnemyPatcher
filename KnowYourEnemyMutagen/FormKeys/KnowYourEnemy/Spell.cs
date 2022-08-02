50a20ac8f6896a76e16066d145d51081a5c8edb5
Drunkender <infernallynxoflife@gmail.com>
2022-08-02 09:57:54 -0600

    Updated ESlified FormIDs
    KYE 9.2.0 ESLified the mod changing the FormIDs this changes the reference IDs.
    Also commented out outdated IDs.

--- KnowYourEnemyMutagen/FormKeys/KnowYourEnemy/Spell.cs
+++ KnowYourEnemyMutagen/FormKeys/KnowYourEnemy/Spell.cs
@@ -9,28 +9,28 @@ namespace Mutagen.Bethesda.FormKeys.SkyrimSE
         public static class Spell
         {
             private static FormLink<ISpellGetter> Construct(uint id) => new FormLink<ISpellGetter>(ModKey.MakeFormKey(id));
-            public static FormLink<ISpellGetter> kye_ab_aquatic => Construct(0xaa37);
-            public static FormLink<ISpellGetter> kye_ab_furred => Construct(0xaa38);
-            public static FormLink<ISpellGetter> kye_ab_fat => Construct(0xaa3b);
-            public static FormLink<ISpellGetter> kye_ab_big => Construct(0xaa3d);
-            public static FormLink<ISpellGetter> kye_ab_small => Construct(0xaa3f);
-            public static FormLink<ISpellGetter> kye_ab_dwarven_machine => Construct(0xaa41);
-            public static FormLink<ISpellGetter> kye_ab_undead => Construct(0xaa43);
-            public static FormLink<ISpellGetter> kye_ab_venomous => Construct(0xaa45);
-            public static FormLink<ISpellGetter> kye_ab_ice_elemental => Construct(0xaa47);
-            public static FormLink<ISpellGetter> kye_ab_fire_elemental => Construct(0xaa49);
-            public static FormLink<ISpellGetter> kye_ab_shock_elemental => Construct(0xaa4b);
-            public static FormLink<ISpellGetter> kye_ab_plant => Construct(0xaa4d);
-            public static FormLink<ISpellGetter> kye_ab_vile => Construct(0xaa4f);
-            public static FormLink<ISpellGetter> kye_ab_skeletal => Construct(0xaa51);
-            public static FormLink<ISpellGetter> kye_ab_troll_kin => Construct(0xaa53);
-            public static FormLink<ISpellGetter> kye_ab_weak_willed => Construct(0xaa55);
-            public static FormLink<ISpellGetter> kye_ab_strong_willed => Construct(0xaa56);
-            public static FormLink<ISpellGetter> kye_ab_cave_dwelling => Construct(0xaa58);
-            public static FormLink<ISpellGetter> kye_ab_vascular => Construct(0xaa5a);
-            public static FormLink<ISpellGetter> kye_ab_supernatural => Construct(0xaa5d);
-            public static FormLink<ISpellGetter> kye_ab_ghostly => Construct(0x60b93);
-            public static FormLink<ISpellGetter> kye_ab_rocky => Construct(0xa28d5);
+            public static FormLink<ISpellGetter> kye_ab_aquatic => Construct(0x81a);
+            public static FormLink<ISpellGetter> kye_ab_furred => Construct(0x81b);
+            public static FormLink<ISpellGetter> kye_ab_fat => Construct(0x81c);
+            public static FormLink<ISpellGetter> kye_ab_big => Construct(0x81d);
+            public static FormLink<ISpellGetter> kye_ab_small => Construct(0x81e);
+            public static FormLink<ISpellGetter> kye_ab_dwarven_machine => Construct(0x81f);
+            public static FormLink<ISpellGetter> kye_ab_undead => Construct(0x820);
+            public static FormLink<ISpellGetter> kye_ab_venomous => Construct(0x821);
+            public static FormLink<ISpellGetter> kye_ab_ice_elemental => Construct(0x822);
+            public static FormLink<ISpellGetter> kye_ab_fire_elemental => Construct(0x823);
+            public static FormLink<ISpellGetter> kye_ab_shock_elemental => Construct(0x824);
+            public static FormLink<ISpellGetter> kye_ab_plant => Construct(0x825);
+            public static FormLink<ISpellGetter> kye_ab_vile => Construct(0x826);
+            public static FormLink<ISpellGetter> kye_ab_skeletal => Construct(0x827);
+            public static FormLink<ISpellGetter> kye_ab_troll_kin => Construct(0x828);
+            public static FormLink<ISpellGetter> kye_ab_weak_willed => Construct(0x829);
+            public static FormLink<ISpellGetter> kye_ab_strong_willed => Construct(0x82a);
+            public static FormLink<ISpellGetter> kye_ab_cave_dwelling => Construct(0x82b);
+            public static FormLink<ISpellGetter> kye_ab_vascular => Construct(0x82c);
+            public static FormLink<ISpellGetter> kye_ab_supernatural => Construct(0x82d);
+            public static FormLink<ISpellGetter> kye_ab_ghostly => Construct(0x847);
+            public static FormLink<ISpellGetter> kye_ab_rocky => Construct(0x865);
         }
     }
 }
